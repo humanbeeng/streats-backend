a7ad285c616f0bb68bb958efbc205d4b9948bf09
Nithin <thenithinraj@gmail.com>
2022-03-20 22:26:13 +0530

    Add StreatsShop repo functions - Get all featured shops - Get shop by shop ID - Get all nearby shops - Get all shops by zip-code

--- src/main/kotlin/app/streat/backend/shop/data/repositories/StreatsShopRepository.kt
+++ src/main/kotlin/app/streat/backend/shop/data/repositories/StreatsShopRepository.kt
@@ -1,12 +1,20 @@
 package app.streat.backend.shop.data.repositories
 
 import app.streat.backend.shop.domain.models.StreatsShop
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint
 import org.springframework.data.mongodb.repository.MongoRepository
+import java.util.*
 
 interface StreatsShopRepository : MongoRepository<StreatsShop, String> {
 
     fun findByShopName(shopName: String): List<StreatsShop>
 
-    fun findStreatsShopById(shopId: String): StreatsShop
+    fun findStreatsShopById(shopId: String): Optional<StreatsShop>
+
+    fun findStreatsShopByZipcode(zipCode: String): List<StreatsShop>
+
+    fun findByFeaturedIsTrue(): List<StreatsShop>
+
+    fun findByLocationNear(geoJsonPoint: GeoJsonPoint): List<StreatsShop>
 
 }
