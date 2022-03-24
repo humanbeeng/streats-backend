f147ea3c79d05b6c95cd7c7885087b0028b3d215
Nithin <thenithinraj@gmail.com>
2022-03-24 22:21:02 +0530

    Add ShopController

---
+++ src/main/kotlin/app/streat/backend/shop/controllers/ShopController.kt
@@ -0,0 +1,40 @@
+package app.streat.backend.shop.controllers
+
+import app.streat.backend.shop.data.repositories.StreatsShopRepository
+import app.streat.backend.shop.domain.models.StreatsShop
+import app.streat.backend.shop.services.ShopService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+/**
+ * TODO : Migrate to AdminService
+ */
+@RestController
+@RequestMapping("/shop")
+class ShopController(private val shopService: ShopService, private val repo: StreatsShopRepository) {
+
+    @GetMapping("/delete/all")
+    fun deleteAll() {
+        return shopService.deleteAllShops()
+    }
+
+    @GetMapping("/test/add")
+    fun addDummyShops(): List<StreatsShop> {
+        return shopService.addDummyShops()
+    }
+
+    @GetMapping("/item/{dishId}")
+    fun getDishDetails(@PathVariable dishId: String): List<StreatsShop> {
+        return repo.findAllByShopItems(dishId)
+    }
+
+    @GetMapping
+    fun allShops(): List<StreatsShop> {
+        return shopService.getAllShops()
+    }
+
+
+}
