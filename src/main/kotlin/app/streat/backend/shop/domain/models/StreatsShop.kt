2920597af3d9a3322be6fcb254f500e6c6cbf6f7
Nithin <thenithinraj@gmail.com>
2022-04-04 08:09:25 +0530

    Remove snake case field names

--- src/main/kotlin/app/streat/backend/shop/domain/models/StreatsShop.kt
+++ src/main/kotlin/app/streat/backend/shop/domain/models/StreatsShop.kt
@@ -5,17 +5,14 @@ import org.springframework.data.mongodb.core.geo.GeoJsonPoint
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexType
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexed
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.mapping.Field
 
 @Document(collection = "shops")
 data class StreatsShop(
     @Id
-    val id: String? = null,
+    val shopId: String? = null,
 
-    @Field("shop_name")
     val shopName: String,
 
-    @Field("shop_owner_phone_number")
     val shopOwnerPhoneNumber: String,
 
     @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
@@ -24,15 +21,10 @@ data class StreatsShop(
     val zipcode: String,
 
 
-    @Field("shop_items")
-//    val shopItems: List<DishItem>,
-
     val shopItems: MutableMap<String, DishItem>,
 
-    @Field("is_takeaway_supported")
     val isTakeawaySupported: Boolean,
 
-    @Field("is_shop_open")
     val isShopOpen: Boolean,
 
     val featured: Boolean = false
