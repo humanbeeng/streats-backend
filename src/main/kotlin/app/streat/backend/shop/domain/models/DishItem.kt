e339b6afad09a90ce6dff790ac027c80ba214e75
Nithin <thenithinraj@gmail.com>
2022-04-04 08:09:10 +0530

    Remove snake case field names

--- src/main/kotlin/app/streat/backend/shop/domain/models/DishItem.kt
+++ src/main/kotlin/app/streat/backend/shop/domain/models/DishItem.kt
@@ -1,10 +1,7 @@
 package app.streat.backend.shop.domain.models
 
-import org.springframework.data.mongodb.core.mapping.Field
-
 data class DishItem(
 
-    @Field("dish_name")
     val dishName: String,
     val price: Double,
 )
