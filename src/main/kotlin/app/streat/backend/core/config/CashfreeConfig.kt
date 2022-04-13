c8c43f7e73d106ed94abe858dd3f4e4027ce3414
Nithin <thenithinraj@gmail.com>
2022-04-13 10:58:37 +0530

    Hotfix Env variables for Cashfree

--- src/main/kotlin/app/streat/backend/core/config/CashfreeConfig.kt
+++ src/main/kotlin/app/streat/backend/core/config/CashfreeConfig.kt
@@ -1,12 +1,10 @@
 package app.streat.backend.core.config
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 
 
 @Component
-@ConfigurationProperties(prefix = "cashfree")
 class CashfreeConfig{
 
     @Value("\${CASHFREE_CLIENT_ID}")
