d86156f5dc515b7d86973db024d49f3fcb86e8a1
Nithin <thenithinraj@gmail.com>
2022-05-02 22:08:55 +0530

    Refactor CartException

--- src/main/kotlin/app/streat/backend/cart/service/exceptions/CartException.kt
+++ src/main/kotlin/app/streat/backend/cart/service/exceptions/CartException.kt
@@ -1,8 +1,12 @@
 package app.streat.backend.cart.service.exceptions
 
 sealed class CartException(override val message: String) : Exception(message) {
+
     class ItemFromDifferentShopException(override val message: String) : CartException(message)
+
     class ItemNotFoundFromShopException(override val message: String) : CartException(message)
+
     class ItemFetchFromCartException(override val message: String) : CartException(message)
+
     class EmptyCartException(override val message: String) : CartException(message)
 }
