06100221dd80596306106f96900c63801b0e25e5
Nithin <thenithinraj@gmail.com>
2022-05-06 13:17:27 +0530

    Refactor Network Headers

---
+++ src/main/kotlin/app/streat/backend/core/util/NetworkConstants.kt
@@ -0,0 +1,8 @@
+package app.streat.backend.core.util
+
+object NetworkConstants {
+
+    const val HEADER_CLIENT_ID = "x-client-id"
+    const val HEADER_CLIENT_SECRET = "x-client-secret"
+    const val HEADER_AUTHORIZATION = "Authorization"
+}
