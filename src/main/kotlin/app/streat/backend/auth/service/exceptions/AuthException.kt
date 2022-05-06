cbfbba9378bcc7e926e0e79b8283f2c943225a87
Nithin <thenithinraj@gmail.com>
2022-05-06 10:34:31 +0530

    Refactor AuthException

--- src/main/kotlin/app/streat/backend/auth/service/exceptions/AuthException.kt
+++ src/main/kotlin/app/streat/backend/auth/service/exceptions/AuthException.kt
@@ -2,8 +2,7 @@ package app.streat.backend.auth.service.exceptions
 
 sealed class AuthException(override val message: String) : Exception(message) {
 
-    class UserNotFoundException(override val message: String) : AuthException(message)
-
+    object UserNotFoundException : AuthException("User not found")
 
 
 }
