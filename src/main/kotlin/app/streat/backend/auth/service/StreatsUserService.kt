74741c799459a6424f6ac5fcce739254b09ab615
Nithin <thenithinraj@gmail.com>
2022-05-14 16:43:05 +0530

    Add addOrderToOrderHistory functionality

--- src/main/kotlin/app/streat/backend/auth/service/StreatsUserService.kt
+++ src/main/kotlin/app/streat/backend/auth/service/StreatsUserService.kt
@@ -2,6 +2,9 @@ package app.streat.backend.auth.service
 
 import app.streat.backend.auth.data.repository.AuthRepository
 import app.streat.backend.auth.domain.models.user.StreatsCustomer
+import app.streat.backend.core.util.CoreConstants
+import app.streat.backend.order.domain.model.order.Order
+import app.streat.backend.order.domain.model.status.OrderStatus
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
@@ -29,12 +32,39 @@ class StreatsUserService(private val repo: AuthRepository) : UserDetailsService 
         return repo.save(streatsCustomer)
     }
 
-    fun getStreatsCustomer(id: String): StreatsCustomer {
+    fun getStreatsCustomerById(id: String): StreatsCustomer {
         return repo.findStreatsCustomerByFirebaseUID(id)
     }
 
     fun checkUserExists(id: String): Boolean {
         return repo.existsStreatsCustomerByFirebaseUID(id)
+    }
+
+    fun addOrderToOrderHistory(order: Order): Order {
+        val userId = order.userId
+        val user = getStreatsCustomerById(userId)
+        user.orders.add(order)
+        updateStreatsCustomer(user)
+        return order
+    }
+
+    fun updateOrderStatusInOrderHistory(userId: String, orderId: String, orderStatus: OrderStatus): StreatsCustomer {
+        val user = getStreatsCustomerById(userId)
+
+        user.orders.find { it.orderId == orderId }?.orderStatus = orderStatus.name
+
+        return updateStreatsCustomer(user)
+    }
+
+    fun clearCart(userId: String) {
+        val user = getStreatsCustomerById(userId)
+        user.cart.cartItems = mutableMapOf()
+        user.cart.totalCost = 0.00
+        user.cart.itemCount = 0
+        user.cart.shopId = CoreConstants.EMPTY
+
+        updateStreatsCustomer(user)
+
     }
 
 }
